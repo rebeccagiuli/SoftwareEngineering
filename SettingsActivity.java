05993aa8fc45cb9d24fb7f2365cde3ea9360b8af
rebeccagiuli <44808001+rebeccagiuli@users.noreply.github.com>
2018-11-29 09:24:03 -0500

    Add files via upload

---
+++ SettingsActivity.java
@@ -0,0 +1,17 @@
+package edu.fsu.cs.cen4020.showMe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
